cce9869ffb4fad9b967e3174441de8e9c5db76a3
Alexey Gaidykov <swiftgaida@list.ru>
2023-02-24 16:49:53 +0300

    parsing json

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var weatherLabel: UILabel!
+    @IBOutlet var getWeatherButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getWeatherButton.addTarget(self, action: #selector(getTapp), for: .touchUpInside)
     }
-
+    
+    @objc func getTapp() {
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
+        guard let url = URL(string: urlString) else { return }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                DispatchQueue.main.async {
+                    self.weatherLabel.text = "\(weather.currentWeather.temperature) °"
+                }
+            } else {
+                print("Error")
+            }
+        }
+        task.resume()
+    }
 
 }
 
